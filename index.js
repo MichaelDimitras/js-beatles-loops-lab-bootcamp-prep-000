5c5e3dd7145b90f884b099b7dc7ef3b54f309207
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-08 21:52:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function theBeatlesPlay(musician, instrument){
-  
+  var plays = [];
+  for(var i = 0; i < musician.length){
+    plays = [...plays, musician[i] + " plays " + instrument[i]];
+    }
 }
+
+
